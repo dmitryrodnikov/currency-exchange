e148e11f55b781863049425ac0247c819d4dc81d
dimarodnikov <dimarodnikov@yandex-team.ru>
2019-09-21 22:46:05 +0300

    Remove unnecessary styles for Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -25,15 +25,6 @@ const StyledInput = styled(NumberFormat)`
     font-weight: bold;
     transition: 300ms background-color;
     
-    /* Remove input controls in Webkit browsers */
-    &::-webkit-inner-spin-button,
-    &::-webkit-outer-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
-    }
-    /* Remove input controls in Firefox */
-    -moz-appearance:textfield;
-    
     &:hover,
     &:focus {
         background-color: ${COLORS.green}10;
