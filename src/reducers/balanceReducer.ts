7c0ff9937d9be56e360d7b88a1f3f37f637e3bd0
dimarodnikov <dimarodnikov@yandex-team.ru>
2019-09-28 13:40:15 +0300

    Reducer for updating balance

--- src/reducers/balanceReducer.ts
+++ src/reducers/balanceReducer.ts
@@ -1,4 +1,6 @@
 import {Currency} from '../domain/Currency';
+import {Action} from 'redux-actions';
+import {BalanceActions, BalanceUpdatePayload, BALANCE_OPERATION} from '../actions/balanceActions';
 
 export type BalanceState = Record<Currency, number>;
 
@@ -10,7 +12,34 @@ const initialState = {
 
 export function balanceReducer(
     state: BalanceState = initialState,
+    action: Action<any>,
 ): BalanceState {
-    return state;
+    switch (action.type) {
+        case BalanceActions.UPDATE_BALANCE:
+            return updateBalance(state, action);
+        default:
+            return state;
+    }
 }
 
+function calculateValue(currentAmount: number, amount: number, operation: BALANCE_OPERATION): number {
+    switch (operation) {
+        case BALANCE_OPERATION.ADD:
+            return currentAmount + amount;
+        case BALANCE_OPERATION.SUBTRACT:
+            return currentAmount - amount;
+    }
+}
+
+function updateBalance(
+    state: BalanceState,
+    action: Action<BalanceUpdatePayload>,
+): BalanceState {
+    const newState = {...state};
+    action.payload.forEach(operation => {
+        const targetCurrency = operation.currency;
+        const currentAmount = newState[targetCurrency];
+        newState[targetCurrency] = calculateValue(currentAmount, operation.amount, operation.operation);
+    });
+    return newState;
+}
